9f3c4dfb15f70d948bc120d03566f7cf2b9dd347
congtargets <884312741@qq.com>
2022-05-11 08:55:19 +0800

    uplload

--- pages/index/index.js
+++ pages/index/index.js
@@ -18,6 +18,7 @@ Page({
     })
   },
   onLoad() {
+    console.log('onLoad')
     if (wx.getUserProfile) {
       this.setData({
         canIUseGetUserProfile: true
@@ -29,6 +30,7 @@ Page({
     wx.getUserProfile({
       desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
       success: (res) => {
+        console.log('展示用户信息')
         console.log(res)
         this.setData({
           userInfo: res.userInfo,
